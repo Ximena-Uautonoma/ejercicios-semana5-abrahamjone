b6af26d74583e15489a94e7ecae888f89ae825f3
abrahamjone <abraham.fernandez@cloud.uautonoma.cl>
2026-04-02 19:47:18 +0000

    falta que se acumule la cuenta de dias

--- Semana5/Ejercicio4.py
+++ Semana5/Ejercicio4.py
@@ -4,6 +4,19 @@ Solicita repetidamente:
 1 si asistió
 0 para terminar
 Al final, muestra cuántos días asistió.'''
+#1 asiste sumar x=cantidad de dias asistido x+1
 
+asistencia=int(input("para marcar su asistencia escriba 1 y no escriba 0:"))
+
+while(asistencia!=2) or(asistencia!=3) or (asistencia!=4)or (asistencia!=5)or (asistencia!=6)or (asistencia!=7)or (asistencia!=8)or (asistencia!=9):
+    if(asistencia ==1):
+       acumulacion=0
+       print("otro dia asistido")
+       acumulacion+1
+       asistencia=int(input("para marcar su asistencia escriba 1 y no escriba 0:"))
+    elif(asistencia==0):
+        print("usted asistio",acumulacion,"dias")
+        break
+        
 
 
